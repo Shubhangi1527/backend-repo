328173828205ec16ffaef81975ace06813e29397
Shubhangi1527 <sataleshubhangi86@gmail.com>
2024-11-10 21:12:24 +0530

    Add files via upload

---
+++ alphaornot.c
@@ -0,0 +1,25 @@
+//check whether character is alphabet or not
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    printf("enter ant character");
+    scanf("%c",&ch);
+
+    if(ch>='a' && ch<='z')
+    {
+        printf("character is an alphabet");
+    }
+    else if("ch>='A' && ch<='Z'")
+    {
+        printf("character is an alphabet");
+    }
+
+    else
+    {
+       printf("character is not an alphabet");
+    }
+}
+
+
